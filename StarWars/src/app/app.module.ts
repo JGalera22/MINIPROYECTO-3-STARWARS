9e0dc5e8ec711f3dff0866692d90f8bdedde1985
JGalera22 <galera221412@gmail.com>
2020-11-23 13:24:34 +0100

    creado de nuevo

---
+++ StarWars/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HttpClientModule } from '@angular/common/http';
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+@Injectable()
+export class ConfigService {
+  constructor(private http: HttpClient) { }
+}
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    [ FlexLayoutModule ]
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
